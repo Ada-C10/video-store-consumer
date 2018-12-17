6f456f8a6d8b1dd341925898533e8ffef3be0ffe
Xtina <2006peacegypsy@gmail.com>
2018-12-17 15:03:00 -0800

    updating

--- src/components/Customers.js
+++ src/components/Customers.js
@@ -1,6 +1,28 @@
 import React, { Component} from 'react';
 
-const Customers = () => {
+class Customer extends Component {
+  componentDidMount() {
+    axios
+      .get(localhost)
+      .then(response => {
+        const cards = response.data.map(card => {
+          const newCard = {
+            ...card.card
+          };
+
+          return newCard;
+        });
+
+        this.setState({
+          cards: cards,
+          errorMessage: null
+        });
+      })
+      .catch(error => {
+        console.log(error.message);
+        this.setState({ errorMessage: error.message });
+      });
+  }
   return  <h2> Customers!!!! </h2>
 }
 
