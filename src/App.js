3764381b26c8f5da480b98ca525c1ec10304371a
Anibel America <ameriabel@gmail.com>
2018-12-17 15:54:42 -0800

    routes in index.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
-import AppRouter from './Components/AppRouter';
+// import './App.css';
+import Customers from './Components/Customers';
+import Library from './Components/Library';
+import Search from './Components/Search';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <AppRouter/>
 
       </div>
     );
   }
 }
+
+module.exports = {
+  Customers,
+  Library,
+  Search
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Customers from './Components/Customers';
+import Library from './Components/Library';
+import Search from './Components/Search';
+
+ReactDOM.render(
+   <Router>
+    <div>
+      <ul>
+        <li>
+          <Link to="/search">Search</Link>
+        </li>
+        <li>
+          <Link to="/library">Library</Link>
+        </li>
+        <li>
+          <Link to="/customers">Customers</Link>
+        </li>
+      </ul>
+
+      <hr />
+
+      <Route path="/search" component={Search} />
+      <Route path="/library" component={Library} />
+      <Route path="/customers" component={Customers} />
+    </div>
+  </Router>,
+  document.getElementById('root')
+  // document.getElementById('app')
+)
+
 registerServiceWorker();
