d3a9af2d707c30fccb75157eb27651aaae822878
Anibel America <ameriabel@gmail.com>
2018-12-17 15:57:39 -0800

    moves routes to app from index

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-// import './App.css';
+import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Customers from './Components/Customers';
 import Library from './Components/Library';
 import Search from './Components/Search';
@@ -9,16 +10,31 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Router>
+         <div>
+           <ul>
+             <li>
+               <Link to="/search">Search</Link>
+             </li>
+             <li>
+               <Link to="/library">Library</Link>
+             </li>
+             <li>
+               <Link to="/customers">Customers</Link>
+             </li>
+           </ul>
+
+           <hr />
+
+           <Route path="/search" component={Search} />
+           <Route path="/library" component={Library} />
+           <Route path="/customers" component={Customers} />
+         </div>
+       </Router>
 
       </div>
     );
   }
 }
-
-module.exports = {
-  Customers,
-  Library,
-  Search
-};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,38 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import Customers from './Components/Customers';
-import Library from './Components/Library';
-import Search from './Components/Search';
-
-ReactDOM.render(
-   <Router>
-    <div>
-      <ul>
-        <li>
-          <Link to="/search">Search</Link>
-        </li>
-        <li>
-          <Link to="/library">Library</Link>
-        </li>
-        <li>
-          <Link to="/customers">Customers</Link>
-        </li>
-      </ul>
-
-      <hr />
-
-      <Route path="/search" component={Search} />
-      <Route path="/library" component={Library} />
-      <Route path="/customers" component={Customers} />
-    </div>
-  </Router>,
-  document.getElementById('root')
-  // document.getElementById('app')
+ReactDOM.render(<App/>, document.getElementById('root')
 )
 
 registerServiceWorker();
