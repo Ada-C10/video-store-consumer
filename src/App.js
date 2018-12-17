ad7ed72f2435555e342cbd29e667e03ee05bbe36
Naheed Arang <arang.na@gmail.com>
2018-12-17 15:50:51 -0800

    create library and movie components, render library

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Library from './components/Library';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <section>
+        <header className="header">
+          <nav>
+            <a href="/html/">HTML</a> |
+            <a href="/css/">CSS</a> |
+            <a href="/js/">JavaScript</a> |
+            <a href="/jquery/">jQuery</a>
+          </nav>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <Library/>
+      </section>
     );
   }
 }
---
+++ src/components/Library.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import axios from 'axios';
+import Movie from './Movie';
+
+import './Library.css';
+
+class Library extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      movies: [],
+    };
+  }
+
+  componentDidMount() {
+    this.getMovies()
+  }
+
+  getMovies = () => {
+    axios.get('http://localhost:3000/')
+    .then((response) => {
+      this.setState({ movies: response.data });
+    })
+    .catch((error) => {
+      this.setState({ error: error.message });
+    });
+  }
+
+  // addCard = (text, emoji) => {
+  //   axios.post(`https://inspiration-board.herokuapp.com/boards/Naheed/movies?text=${text}&emoji=${emoji}`)
+  //   .then((response) => {
+  //     this.getCards()
+  //   });
+  // }
+  //
+  // deleteCard = (id) => {
+  //   axios.delete(`https://inspiration-board.herokuapp.com/cards/${id}`, id)
+  //   .then((response) => {
+  //     this.getCards()
+  //   });
+  // }
+
+  render() {
+    const allMovies = this.state.movies.map((movie) => {
+      return <Movie
+        key={movie.id}
+        id={movie.id}
+        title={movie.title}
+        overview={movie.overview}
+        release_date={movie.release_date}
+        image_url={movie.image_url}
+        external_id={movie.external_id}/>
+    });
+
+    return (
+      <div >
+        <div className="library">
+          {allMovies}
+        </div>
+      </div>
+    )
+  }
+}
+
+Library.propTypes = {
+
+};
+
+export default Library;
