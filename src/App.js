c6767a48a61ed3d65828c31b7f561e73540735fe
Goeun Park <goeuntothepark@gmail.com>
2018-12-17 16:26:16 -0800

    spacing and prelim visuals

--- src/App.js
+++ src/App.js
@@ -6,32 +6,19 @@ class App extends Component {
   render() {
     return (
       <Router>
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Video Consumer Placeholder Title</h1>
-          <div className = "navbar">
-            <nav>
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">Video Consumer Placeholder Title</h1>
+            <nav className="navbar">
               <ul>
-                <li>
-                  <Link to="/">Homepage</Link>
-                </li>
-                <li>
-                  <Link to="/library">Library</Link>
-                </li>
-                <li>
-                  <Link to="/search">Search</Link>
-                </li>
-                <li>
-                  <Link to="/customers">Customers</Link>
-                </li>
+                <li><Link to="/">Homepage</Link></li>
+                <li><Link to="/library">Library</Link></li>
+                <li><Link to="/search">Search</Link></li>
+                <li><Link to="/customers">Customers</Link></li>
               </ul>
             </nav>
-          </div>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+          </header>
+        </div>
       </Router>
     );
   }
