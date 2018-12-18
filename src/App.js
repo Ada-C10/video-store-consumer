73b313e44d9a9c5da28d3a004e5424d740074ce6
Chantelle Belic <chantelle.belic@gmail.com>
2018-12-18 11:47:25 -0800

    started checkout movie

--- src/App.js
+++ src/App.js
@@ -29,48 +29,53 @@ class App extends Component {
     });
   };
 
+  checkoutMovie = () => {
+    // http://localhost:3000/rentals/Jaws/check-out?customer_id=1&due_date=12/11/2019
+    const rentalUrl = `${this.url}rentals/${this.state.selectMovie.title}/check-out?`;
+    axios.post(rentalUrl, {customer_id: this.state.selectCustomer.id, due_date: Date.today + 7})
+  }
+
   url = "http://localhost:3000/"
 
   render() {
     return (
       <div className="App">
         <Router>
-         <div>
-           <ul>
-             <li>
-               <Link to="/search">Search</Link>
-             </li>
-             <li>
-               <Link to="/library">Library</Link>
-             </li>
-             <li>
-               <Link to="/customers">Customers</Link>
-             </li>
-           </ul>
+          <div>
+            <ul>
+              <li>
+                <Link to="/search">Search</Link>
+              </li>
+              <li>
+                <Link to="/library">Library</Link>
+              </li>
+              <li>
+                <Link to="/customers">Customers</Link>
+              </li>
+            </ul>
+            <div>
+              <p>Selected Movie</p>
+              {this.state.selectedMovie && <p>{this.state.selectedMovie.title}</p>}
+            </div>
+            <div>
+              <p>Selected Customer</p>
+              {this.state.selectedCustomer && <p>{this.state.selectedCustomer.name}</p>}
+            </div><button onClick={checkoutMovie}>Check Out New Rental</button>
+          </div>
+          <Route path="/search" component={Search} />
+          <Route
+            path="/library"
+            render={() => <Library selectMovieCallback={this.selectMovie} baseUrl={this.url}/>}
+            />
+          <Route path='/customers'
+            component={() => <Customers baseUrl={this.url} selectCustomerCallback={this.selectCustomer}/>}
+            />
+        </div>
+      </Router>
 
-           <div>
-             <p>Selected Movie</p>
-             {this.state.selectedMovie && <p>{this.state.selectedMovie.title}</p>}
-           </div>
-           <div>
-             <p>Selected Customer</p>
-             {this.state.selectedCustomer && <p>{this.state.selectedCustomer.name}</p>}
-           </div>
-
-           <Route path="/search" component={Search} />
-           <Route
-             path="/library"
-             render={() => <Library selectMovieCallback={this.selectMovie} baseUrl={this.url}/>}
-           />
-           <Route path='/customers'
-             component={() => <Customers baseUrl={this.url} selectCustomerCallback={this.selectCustomer}/>}
-             />
-         </div>
-        </Router>
-
-      </div>
-    );
-  }
+    </div>
+  );
+}
 }
 
 export default App;
