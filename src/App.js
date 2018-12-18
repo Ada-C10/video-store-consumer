805771221c9b93f6fbdeee0bf771e6ffdf623704
Kay <kayxn23@gmail.com>
2018-12-18 12:54:38 -0800

    added SearchBar.js to App for search bar funcationality still with bug

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
- import CustomersCollection from './components/CustomersCollection.js';
- import RentalLibrary from './components/RentalLibrary.js';
+import CustomersCollection from './components/CustomersCollection.js';
+import RentalLibrary from './components/RentalLibrary.js';
+import SearchBar from './components/SearchBar.js';
 import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 
 
@@ -26,6 +27,13 @@ class App extends Component {
         <Route path="/customers" component={CustomersCollection }/>
         <Link to={'/library'}>Rental Library</Link>
         <Route path="/library" component={RentalLibrary }/>
+
+        <section className="search-bar">
+          <SearchBar onSearchChange={this.onSearchChange} />
+        </section>
+        
+        <Link to={'/search'}>Search Bar </Link>
+        <Route path="/search" component={ SearchBar }/>
 
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
---
+++ src/components/SearchBar.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './SearchBar.css';
+
+
+class SearchBar extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchValue: '',
+    };
+  }
+
+  onSearchChange = (event) => {
+    console.log("printing event in onSearchChange",event);
+    this.setState({
+      searchValue: event.target.value,
+    });
+
+    this.props.onSearchChange(event.target.value);
+  }
+
+  render() {
+    return (
+      <section>
+        <input
+          onChange={this.onSearchChange}
+          value={this.state.searchValue}
+          name="search-bar"
+          className="search-bar"
+          placeholder="Filter Movies"
+        />
+      </section>
+    );
+  }
+}
+
+SearchBar.propTypes = {
+  onSearchChange: PropTypes.func.isRequired,
+};
+export default SearchBar ;
