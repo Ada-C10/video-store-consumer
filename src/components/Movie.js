1b9fee470c6c67a2eaae49a9c7fb77f2991b0e79
Aurea <fu.sakura@gmail.com>
2018-12-17 16:11:35 -0800

    add feature to see list of all movies in library

---
+++ src/components/Movie.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './Movie.css';
+
+const Movie = (props) => {
+
+  const { title, overview, image_url, release_date } = props.movie;
+
+  return(
+    <div className="movie">
+      <h3>{title} </h3>
+      <div>{release_date}</div>
+      <img src={image_url} alt={title}/>
+      <p>{overview}</p>
+    </div>
+  );
+};
+
+Movie.propTypes = {
+  movie: PropTypes.object.isRequired
+};
+
+export default Movie;
