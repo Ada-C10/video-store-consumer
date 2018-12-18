74bad702ceaf9cf1fdc21e7c675548d0e758ba15
Amber Lynn <griffiam@gmail.com>
2018-12-17 17:09:41 -0800

    added customer component

--- src/components/Library.js
+++ src/components/Library.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 import Movie from './Movie';
+import Customer from './Customer';
 
 import './Library.css';
 
@@ -11,6 +12,7 @@ class Library extends Component {
 
     this.state = {
       movies: [],
+      customers: [],
     };
   }
 
@@ -22,6 +24,16 @@ class Library extends Component {
     axios.get('http://localhost:3000/')
     .then((response) => {
       this.setState({ movies: response.data });
+    })
+    .catch((error) => {
+      this.setState({ error: error.message });
+    });
+  }
+
+  getCustomers = () => {
+    axios.get('http://localhost:3000/')
+    .then((response) => {
+      this.setState({ customers: response.data });
     })
     .catch((error) => {
       this.setState({ error: error.message });
@@ -54,10 +66,27 @@ class Library extends Component {
         external_id={movie.external_id}/>
     });
 
+    const allCustomers = this.state.customers.map((customer) => {
+      return <Customer
+        key={customer.id}
+        id={customer.id}
+        name={customer.name}
+        address={customer.address}
+        city={customer.city}
+        state={customer.state}
+        postal_code={customer.postal_code}
+        phone={customer.phone}
+        account_credit={customer.account_credit}
+        created_at={customer.created_at}/>
+    });
+
     return (
       <div >
         <div className="library">
           {allMovies}
+        </div>
+        <div className="customers">
+          {allCustomers}
         </div>
       </div>
     )
