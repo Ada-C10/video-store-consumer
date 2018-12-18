1deed10e287c0888fb6946046a275d7f49d128b4
Kay <kayxn23@gmail.com>
2018-12-17 16:32:56 -0800

    connected customers to app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Customers from './components/Customers.js';
+// import Customers from './components/Customers.js';
 import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+
+
+const Customers1 = () => <h2>customers</h2>;
+
+
 
 class App extends Component {
 
@@ -16,14 +21,9 @@ class App extends Component {
       <Router>
 
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
 
-        <Customers />
-        <Link to={'/customers'}></Link>
-        <Route path="/customers" component={Customers}/>
+        <Link to={'/customers1'}>Customers</Link>
+        <Route path="/customers1" component={Customers1}/>
 
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
