a31dd2d55eb18a8829370cafc0e167f2f2d97c5b
Katricia Smith <smithkatricia@gmail.com>
2018-12-18 11:14:47 -0800

    Add onSearch callback function and onChange handler to form

--- src/components/Search.js
+++ src/components/Search.js
@@ -6,6 +6,18 @@ import './styles/Search.css';
 class Search extends Component {
   constructor() {
     super();
+
+    this.state = {
+      query: "",
+    }
+  }
+
+  onSearch = (event) => {
+    console.log(`User searched for ${event.target.value}`);
+
+    this.setState({
+     query: event.target.value,
+   });
   }
 
   render () {
@@ -14,7 +26,8 @@ class Search extends Component {
         <form className="movie-search-form">
           <div className="movie-search-form-container">
             <label htmlFor="searchQuery"></label>
-            <input name="query" placeholder="Search by Movie"/>
+            <input name="query" placeholder="Search by Movie"
+                onChange={this.onSearch}/>
           </div>
         </form>
       </div>
