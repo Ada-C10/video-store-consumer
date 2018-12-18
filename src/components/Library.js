41965867062f43571129467c4289d1f26aba5c22
Lindsay Terchin <lterchin@gmail.com>
2018-12-18 11:27:16 -0800

    added components

---
+++ src/components/Library.js
@@ -0,0 +1,26 @@
+// import axios from 'axios';
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+
+// import './Library.css';
+
+class Library extends Component {
+ constructor(props) {
+   super(props);
+ }
+
+ render() {
+
+   return (
+     <div>
+      Library component
+     </div>
+   )
+ }
+}
+
+Library.propTypes = {
+
+};
+
+export default Library;
--- src/components/LibraryLink.js
+++ src/components/LibraryLink.js
@@ -1,12 +1,38 @@
 // import axios from 'axios';
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import Library from './Library';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
+
 
 // import './LibraryButton.css';
 
 class LibraryLink extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      movies: [
+        {
+          title: "Psycho",
+          overview: "When larcenous real estate clerk Marion Crane goes on the lam with a wad of cash and hopes of starting a new life, she ends up at the notorious Bates Motel.",
+          release_date: "1960-06-16",
+          image_url:"https://image.tmdb.org/t/p/w185/81d8oyEFgj7FlxJqSDXWr8JH8kV.jpg"
+        },
+        {
+          title: "Jaws",
+          overview: "An insatiable great white shark terrorizes the townspeople of Amity Island, The police chief, an oceanographer and a grizzled shark hunter seek to destroy the bloodthirsty beast.",
+          release_date: "1975-06-18",
+          image_url: "https://image.tmdb.org/t/p/w185/l1yltvzILaZcx2jYvc5sEMkM7Eh.jpg"
+        },
+        {
+          title: "Movie3",
+          overview: "Movie Overview",
+          release_date: "1975-06-18",
+          image_url: "https://image.tmdb.org/t/p/w185/l1yltvzILaZcx2jYvc5sEMkM7Eh.jpg"
+        },
+      ]
+    }
   }
 
   displayLibrary = () => {
@@ -14,10 +40,21 @@ class LibraryLink extends Component {
   }
 
   render() {
+    const { movies } = this.state
+    console.log(movies)
+
+    const allMovies = movies.map((movie) => {
+      <div key={movie.id}>
+        {movie.title}
+      </div>
+    })
 
     return (
       <div>
-        <p>Library</p>
+
+            <Library>
+              {allMovies}
+            </Library>
       </div>
     )
   }
