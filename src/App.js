382fbd2d4aae82717daf57568900fbd0b3c3bce2
Katricia Smith <smithkatricia@gmail.com>
2018-12-17 16:09:34 -0800

    Add react router Router and Links to

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Video Consumer Placeholder Title</h1>
+          <div className = "navbar">
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">Homepage</Link>
+                </li>
+                <li>
+                  <Link to="/library">Library</Link>
+                </li>
+                <li>
+                  <Link to="/search">Search</Link>
+                </li>
+                <li>
+                  <Link to="/customers">Customers</Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
+      </Router>
     );
   }
 }
