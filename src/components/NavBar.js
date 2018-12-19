b32fc66adc6acdf37f840cbf6c410f1aebf8949f
Hayden Walls <hwilliams.deva@gmail.com>
2018-12-19 15:35:27 -0800

    fix navbar <link>s

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,35 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 
 
-class NavBar extends Component {
-  render() {
-    return (
-      <div className='navbar'>
-        <nav className='navbar__nav'>
-          <p>
-          <Link to='/search'>
-            Search
+const NavBar = () => {
+  return (
+    <div className='navbar'>
+      <nav className='navbar__nav'>
+        <p>
+        <Link to='/search'>
+          Search
+        </Link>
+        </p>
+        <p>
+          <Link to='/library'>
+            Library
           </Link>
-          </p>
-          <p>
-            <Link to='/library'>
-              Library
-            </Link>
-          </p>
-          <p>
-            <Link to='/customers'>
-              Customers
-            </Link>
-          </p>
-        </nav>
-
-
-
-      </div>
-    );
-  }
+        </p>
+        <p>
+          <Link to='/customers'>
+            Customers
+          </Link>
+        </p>
+      </nav>
+    </div>
+  )
 }
 
 export default NavBar;
