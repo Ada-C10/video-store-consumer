8bc579f08f57d7712424956fc4a070e7579cf2f8
Katricia Smith <smithkatricia@gmail.com>
2018-12-19 10:05:38 -0800

    Change SearchHit render to mirror Movie render format

--- src/components/SearchHit.js
+++ src/components/SearchHit.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import './styles/Movie.css';
 
 
 const SearchHit = ({id, title, overview, release_date, image_url}) => {
 
+  const image_alt_tag = `Movie poster for ${title}`;
+
   return (
-    <div>
-      <ul>
-        <li>{title}</li>
-      </ul>
+    <div className="movie-item">
+      <img className="movie-item__image" src={image_url} alt={image_alt_tag}/>
+      <div className="movie-item__details">
+        <h2>{title}</h2>
+        <h3>{release_date}</h3>
+        <p>{overview}</p>
+      </div>
+      <button className="movie-item__select-button">Select for rental</button>
     </div>
   );
 };
