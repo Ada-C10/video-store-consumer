9faf3c2f653a2c3f185dd6c7c6a1042b49ae15dd
Karis Kim <ouniiq@hotmail.com>
2018-12-19 08:54:57 -0800

    did a basic readjustment of how the header should be aligned, put them all in a list and used flex to make it into a row

--- src/Components/Library.js
+++ src/Components/Library.js
@@ -4,10 +4,8 @@ import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import VideoCollection from './VideoCollection'
 import Search from './Search'
 import CustomerCollection from './CustomerCollection';
-// import Search from '/Search';
 
 class Library extends React.Component {
-
 
   constructor() {
     super();
@@ -19,35 +17,43 @@ class Library extends React.Component {
 
   }
 
-
-
-
   render() {
     return (
       <Router>
         <section className="App">
           <header className="header">
             <nav>
-              <button>
-                <Link to="/VideoCollection">Library</Link>
-              </button>
-              <button>
-                <Link to="/CustomerCollection">Customers</Link>
-              </button>
-              <button>
-                <Link to="/Search">Search</Link>
-              </button>
+              <ul>
+                <li>
+                  <button>
+                    <Link to="/VideoCollection">Library</Link>
+                  </button>
+                </li>
+                <li>
+                  <button>
+                    <Link to="/CustomerCollection">Customers</Link>
+                  </button>
+                </li>
+                <li>
+                  <button>
+                    <Link to="/Search">Search</Link>
+                  </button>
+                </li>
+                <li>
+                  <label>Selected Movie</label>
+                  <p>{this.state.selectedMovie}</p>
+                </li>
+                <li>
+                  <label>Selected Customer</label>
+                  <p>{this.state.selectedCustomer}</p>
+                </li>
+                <li>
+                  <button>
+                    <Link to="/">Check Out New Rental</Link>
+                  </button>
+                </li>
+              </ul>
             </nav>
-
-            <div>
-              <label>Selected Movie</label>
-              <p>{this.state.selectedMovie}</p>
-            </div>
-
-            <div>
-              <label>Selected Customer</label>
-              <p>{this.state.selectedCustomer}</p>
-            </div>
           </header>
 
           <section className="showPageToUser">
