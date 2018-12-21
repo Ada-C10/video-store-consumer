1a3ec20588d5482498d8f73f97c1da905308ea18
Chantelle Belic <chantelle.belic@gmail.com>
2018-12-21 11:08:22 -0800

    changed n in movies

--- src/Components/Library.js
+++ src/Components/Library.js
@@ -33,7 +33,7 @@ class Library extends Component {
 
   componentDidMount() {
 
-    const VIDEO_STORE_API = this.props.baseUrl + 'movies';
+    const VIDEO_STORE_API = this.props.baseUrl + 'movies?n=500';
     this.props.setStatusMessageCallback("Loading Libary...");
 
     axios.get(VIDEO_STORE_API)
