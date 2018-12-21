2bc417d0bdd0bf43f95e6c125e9678c80bc3c1cc
Katricia Smith <smithkatricia@gmail.com>
2018-12-20 22:46:14 -0800

    Move conditional button handling from clickHandler to render. make clickHandler two separate callback functions

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -9,19 +9,28 @@ class Movie extends Component {
     super(props);
   }
 
-  clickHandler = () => {
-    console.log("in movie click handler", this.props);
-    const task = this.props.message;
-  // if movie is not in library, clickHander is addToLibray
-    if (task.includes("Add")) {
+  // clickHandler = () => {
+  //   console.log("in movie click handler", this.props);
+  //   const task = this.props.message;
+  // // if movie is not in library, clickHander is addToLibray
+  //   if (task.includes("Add")) {
+  //   this.props.addToLibraryCallback(this.props);
+  // // if movie is already in library, handler is to addRental
+  //   } else if (task.includes("Select")) {
+  //     this.props.addRentalCallback(this.props);
+  //   }
+  addtoLibraryHandler = () => {
     this.props.addToLibraryCallback(this.props);
-  // if movie is already in library, handler is to addRental
-    } else if (task.includes("Select")) {
-      this.props.addRentalCallback(this.props);
-    }
+  }
+
+  selectMovieHandler = () => {
+    this.props.selectMovieCallback(this.props);
+  }
 
   render () {
     const image_alt_tag = `Movie poster for ${this.props.title}`;
+    const task = this.props.message;
+    // change conditional to be if is included in library rather than on message?
 
     return (
       <div className="movie-item">
@@ -30,8 +39,13 @@ class Movie extends Component {
           <h2>{this.props.title}</h2>
           <h4>{this.props.release_date}</h4>
           <p>{this.props.overview}</p>
-          <Button color="secondary"  className="movie-item__select-button" onClick={this.clickHandler}>
+          {task.includes("Add")? (
+          <Button color="secondary"  className="movie-item__select-button" onClick={this.addtoLibraryHandler}>
              {this.props.message}</Button>
+         ) : (
+           <Button color="secondary"  className="movie-item__select-button" onClick={this.selectMovieHandler}>
+              {this.props.message}</Button>
+         )}
         </div>
       </div>
     )
