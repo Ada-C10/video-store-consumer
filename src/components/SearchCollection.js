f08fb40b19fc7e54611799f02b7c2a25280c86ba
xLJohnsonx <lizjohnsonemail@gmail.com>
2018-12-20 22:35:08 -0800

    trying error.errors

--- src/components/SearchCollection.js
+++ src/components/SearchCollection.js
@@ -39,34 +39,21 @@ class SearchCollection extends Component {
   addToLibrary = (movie) => {
     console.log(movie);
     axios.post(TMDBMOVIEURL + `movies`, movie)
-        .then((response) => {
-          console.log(response);
-          console.log(response.data);
-          this.setState({
-            message: `${movie.title} has been added to Rent-O-Rama's Collection`
-          })
-        })
-        .catch((error) => {
-          // Error
-          if (error.response) {
-            // The request was made and the server responded with a status code
-            // that falls out of the range of 2xx
-            console.log(error.response.data);
-            console.log(error.response.status);
-            console.log(error.response.headers);
-          } else if (error.request) {
-            // The request was made but no response was received
-            // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-            // http.ClientRequest in node.js
-            console.log(error.request);
-          } else {
-            // Something happened in setting up the request that triggered an Error
-            console.log('Error', error.message);
-          }
-          console.log(error.config);
-        });
-  }
-
+    .then((response) => {
+      console.log(response);
+      console.log(response.data);
+      this.setState({
+        message: `${movie.title} has been added to Rent-O-Rama's Collection`
+      })
+    })
+    .catch((error) => {
+      console.log(error.errors.message);
+      console.log(error.message);
+      this.setState({
+        message: error.message,
+      })
+    });
+  };
 
   renderMovieList = () => {
     let movies = this.state.matchingMovies;
