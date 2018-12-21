41e4efd134333d41f02ca40575e89b33362895c8
Goeun Park <goeuntothepark@gmail.com>
2018-12-20 17:48:06 -0800

    add alert for loading list of customers

--- src/components/CustomerList.js
+++ src/components/CustomerList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Alert } from 'reactstrap';
 import './styles/Customer.css';
 import axios from 'axios';
 import Customer from './Customer';
@@ -15,14 +16,18 @@ class CustomerList extends Component {
   }
 
   componentDidMount () {
-    const URL = 'http://localhost:3000/customers'
+    const URL = 'http://localhost:3000/customers';
+    this.setState({alert: 'Loading directory of customers...'});
 
     axios.get(URL)
     .then((response) => {
       const allCustomers = response.data.map((customer, i ) => {
         return <Customer key={i} {...customer} />
       });
-      this.setState({customers: allCustomers});
+
+      const alertMessage = `Successfully loaded ${response.data.length} customers from the customers directory`
+      this.setState({customers: allCustomers,
+      alert: alertMessage});
     })
     .catch((error) => {
       this.setState({error: error.message});
@@ -32,7 +37,10 @@ class CustomerList extends Component {
   render () {
     return (
       <div>
-        {this.state.customers}
+        <Alert color="success">{this.state.alert}</Alert>
+        <div>
+          {this.state.customers}
+        </div>
       </div>
     )
   }
