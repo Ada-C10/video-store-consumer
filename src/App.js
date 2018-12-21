f5ec44b1383968584b94002d3d0f49b8e7e29e41
Katricia Smith <smithkatricia@gmail.com>
2018-12-21 11:24:29 -0800

    Edit starting App selectedMovie state to None

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      selectedMovie: "falk",
+      selectedMovie: "None",
       selectedCustomer: "None",
     };
   }
