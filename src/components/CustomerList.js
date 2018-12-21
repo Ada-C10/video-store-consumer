e9e9169d670352ba219b6cbcfa3a4fb8ce6f5411
Goeun Park <goeuntothepark@gmail.com>
2018-12-20 23:52:16 -0800

    add external component ScrollUpButton to make scrollling up more user friendly

--- src/components/CustomerList.js
+++ src/components/CustomerList.js
@@ -4,6 +4,7 @@ import { Alert } from 'reactstrap';
 import './styles/CustomerList.css';
 import axios from 'axios';
 import Customer from './Customer';
+import ScrollUpButton from "react-scroll-up-button";
 
 
 class CustomerList extends Component {
@@ -22,7 +23,7 @@ class CustomerList extends Component {
     axios.get(URL)
     .then((response) => {
       const allCustomers = response.data.map((customer, i ) => {
-        return <Customer key={i} {...customer} />
+        return <Customer selectCustomerCallback={this.props.selectCustomerCallback} key={i} {...customer} />
       });
 
       const alertMessage = `Successfully loaded ${response.data.length} customers from the customers directory`
@@ -36,12 +37,17 @@ class CustomerList extends Component {
 
   render () {
     return (
-      <div className="customer-list__container">
-        <Alert color="success">{this.state.alert}</Alert>
-        <div className="customer-list">
-          {this.state.customers}
+      <div>
+        <div className="customer-list__container">
+          <Alert color="success">{this.state.alert}</Alert>
+          <div className="customer-list">
+            {this.state.customers}
+          </div>
         </div>
+        <ScrollUpButton/>
       </div>
+
+      
     )
   }
 }
